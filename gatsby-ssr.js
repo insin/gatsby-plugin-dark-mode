0fa6f0415d07b4615488cc5132ef713e08f808dc
Robbin Janssen <robbinjanssen@gmail.com>
2019-08-09 15:34:35 +0200

    Use not null compare

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -17,7 +17,7 @@ void function() {
   function setTheme(newTheme) {
     window.__theme = newTheme
     preferredTheme = newTheme
-    document.body.className.match(/dark|light/g).length > 0 
+    document.body.className.match(/dark|light/g) !== null 
       ? document.body.classList.replace(/dark|light/g, newTheme)
       : document.body.classList.add(newTheme)
     window.__onThemeChange(newTheme)
